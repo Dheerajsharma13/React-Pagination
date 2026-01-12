73c3aa39e0a0fbb66a002035c8977780e3ea5b1a
dheeraj sharma <dheeraj.sharmaa13@gmail.com>
2026-01-13 01:42:32 +0530

    remove axios

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ const App = () => {
 
     useEffect(() => {
       async function fetchData() {
-        const response = await axios.get(
+        const response = await fetch(
             "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
         );
-
-        setCoinsData(response.data);
+        const data = await response.json();
+        setCoinsData(data);
       }
 
       fetchData();
